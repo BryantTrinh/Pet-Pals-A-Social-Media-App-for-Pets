393df929f964c2abbf6a06f164f2fd6d773d24d4
Bryant Trinh <trinhbryant@gmail.com>
2023-01-30 15:16:20 -0800

    added socket.io dependency, Chat.js in client, socket.io in server.js

---
+++ client/src/Chat.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const chat = () => {
+	const [socket] = useState(io(":3000"));
+	const [message, setMessage] = useState(" ");
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		socket.on("receiveMessage", (message) => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
+
+	const sendMessage = () => {
+		socket.emit("sendMessage", message);
+		setMessage(" ");
+	};
+
+	return (
+		<div>
+			<ul>
+				{messages.map((message, index) => (
+					<li key={index}>{message}</li>
+				))}
+			</ul>
+			<input
+				type="text"
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+			/>
+			<button onClick={sendMessage}>Send</button>
+		</div>
+	);
+};
+
+export default Chat;
