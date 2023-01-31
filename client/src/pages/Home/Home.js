90dece694cb736ad7ed0c183a3bc09a4008c2104
monysary <sary.mony@gmail.com>
2023-01-31 10:19:54 -0800

    moved login jsx from Home.js to Login.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,31 +1,9 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import PetsIcon from '@mui/icons-material/Pets';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 
-import Register from '../Register';
+import Login from '../Login';
 
 export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
-  // Show sign up modal use state
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
     // Image left
@@ -44,68 +22,7 @@ export default function SignInSide() {
           backgroundPosition: 'center',
         }}
       />
-      {/* Login form */}
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square
-        sx={{
-          backgroundColor: '#F8F5F2'
-        }}
-      >
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: '#F8F5F2'
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <PetsIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            LOG IN
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2, backgroundColor: '#405C96' }}
-            >
-              LOG IN
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link onClick={handleOpen} variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Grid>
-      <Register handleClose={handleClose} open={open}/>
+      <Login />
     </Grid>
   );
 }
