b86afb8b9efc8f7bacd2642a0475e182b5d51fcb
Brian Wang <wangbrian26@gmail.com>
2023-01-31 15:03:35 -0800

    updated

--- client/src/pages/Logout/Logout.js
+++ client/src/pages/Logout/Logout.js
@@ -2,49 +2,50 @@ import React, { useState, useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import { Button } from "@mui/material";
+import auth from "../../utils/auth.js";
 
 const LOGOUT = gql`
-	mutation Logout {
-		logout
-	}
+  mutation Logout {
+    logout
+  }
 `;
 
 function LogoutForm() {
-	const [logout, { data }] = useMutation(LOGOUT);
-	const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const [logout, { data }] = useMutation(LOGOUT);
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
 
-	useEffect(() => {
-		if (data) {
-			AuthService.logout();
-			setIsLoggedOut(true);
-		}
-	}, [data]);
+  useEffect(() => {
+    if (data) {
+      auth.logout();
+      setIsLoggedOut(true);
+    }
+  }, [data]);
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		await logout();
-	};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await logout();
+  };
 
-	if (isLoggedOut) {
-		return <p>You have successfully logged out.</p>;
-	}
+  if (isLoggedOut) {
+    return <p>You have successfully logged out.</p>;
+  }
 
-	return (
-		<Button variant="outlined"
-			sx={{
-				color: "white",
-				borderColor: "white",
-				ml: "20px",
-				"&:hover": {
-					color: "#DE4567",
-					borderColor: "#DE4567",
-				}
-			}}
-
-		>
-			Logout
-		</Button>
-	);
+  return (
+    <Button
+      variant="outlined"
+      sx={{
+        color: "white",
+        borderColor: "white",
+        ml: "20px",
+        "&:hover": {
+          color: "#DE4567",
+          borderColor: "#DE4567",
+        },
+      }}
+    >
+      Logout
+    </Button>
+  );
 }
 
 export default LogoutForm;
