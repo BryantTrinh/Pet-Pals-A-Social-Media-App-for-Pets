5ba038816e19d625641dc33634f87a1fe438a69c
monysary <sary.mony@gmail.com>
2023-02-01 13:29:41 -0800

    styled feed page

--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -7,95 +7,116 @@ import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ChatIcon from '@mui/icons-material/Chat';
 
 export default function RecipeReviewCard() {
     return (
-        <Grid container direction="column" alignItems="center" sx={{ marginTop: "20px" }}>
+        <Grid container justifyContent="center" gap={4} sx={{ marginTop: "20px", padding: "0 20px" }}>
             {/* Map over card */}
-            <Card sx={{ width: 345, marginBottom: "20px" }}>
-                <CardHeader
-                    title="Chorizo"
-                />
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
-                    alt="pet profile"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        Location
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Breed/Species
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Pictures
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <ChatIcon />
-                    </IconButton>
-                </CardActions>
-            </Card>
-            <Card sx={{ width: 345, marginBottom: "20px" }}>
-                <CardHeader
-                    title="Chorizo"
-                />
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
-                    alt="pet profile"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        Location
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Breed/Species
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Pictures
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <ChatIcon />
-                    </IconButton>
-                </CardActions>
-            </Card>
-            <Card sx={{ width: 345, marginBottom: "20px" }}>
-                <CardHeader
-                    title="Chorizo"
-                />
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
-                    alt="pet profile"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        Location
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Breed/Species
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Pictures
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <ChatIcon />
-                    </IconButton>
-                </CardActions>
-            </Card>
+            <Grid item xs={12} sm={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        title="Chorizo"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
+                        alt="pet profile"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            Location
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Breed/Species
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <ChatIcon />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        title="Chorizo"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
+                        alt="pet profile"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            Location
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Breed/Species
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <ChatIcon />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        title="Chorizo"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
+                        alt="pet profile"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            Location
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Breed/Species
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <ChatIcon />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        title="Chorizo"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
+                        alt="pet profile"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            Location
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Breed/Species
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <ChatIcon />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </Grid>
         </Grid>
     );
 }
