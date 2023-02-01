1e563a9d2b97f2d5fb4c1481cb4a546d7162edeb
cytoplasma <jpiromg@gmail.com>
2023-02-01 11:25:05 -0800

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -49,29 +49,14 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-<<<<<<< HEAD
-          {/* <Backdrop sx={{ backdropFilter: "blur(3px)" }}
-            open={open}
-            onClick={handleClose}
-          > */}
-            <Navbar />
-            <ChatBox handleToggle={handleToggle} />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          {/* </Backdrop> */}
-=======
           <Navbar />
           <ChatBox />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
           </Routes>
->>>>>>> d90f4e5b29dc2c902f7507d23f369ed2093e2ce3
         </div>
       </Router>
     </ApolloProvider>
