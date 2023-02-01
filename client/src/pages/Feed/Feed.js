82fab9f852e0489dde83aaaf552cf7e971a64829
monysary <sary.mony@gmail.com>
2023-02-01 12:04:11 -0800

    committing code

--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -6,15 +6,12 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Grid } from '@mui/material';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -35,26 +32,15 @@ export default function RecipeReviewCard() {
     };
 
     return (
-        <Grid container justifyContent="center">
+        <Grid container justifyContent="center" sx={{ marginTop: "20px" }}>
             <Card sx={{ maxWidth: 345 }}>
                 <CardHeader
-                    avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            R
-                        </Avatar>
-                    }
-                    action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
-                    }
-                    title="Shrimp and Chorizo Paella"
-                    subheader="September 14, 2016"
+                    title="Chorizo"
                 />
                 <CardMedia
                     component="img"
                     height="194"
-                    image="/static/images/cards/paella.jpg"
+                    image="https://repository-images.githubusercontent.com/260096455/47f1b200-8b2e-11ea-8fa1-ab106189aeb0"
                     alt="Paella dish"
                 />
                 <CardContent>
