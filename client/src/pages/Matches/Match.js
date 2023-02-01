e9b9ccfbb0c733038a679261b43540be05de5063
Brian Wang <wangbrian26@gmail.com>
2023-02-01 14:24:09 -0800

    ending this branch

--- client/src/pages/Matches/Match.js
+++ client/src/pages/Matches/Match.js
@@ -1,23 +1,25 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../../utils/queries";
 import { getDistance } from "geolib";
+import auth from "../../utils/auth.js";
 
-function Match() {
-  const { loading, data } = useQuery(QUERY_USER);
-  const userLocation = data?.location || "";
-  if (userLocation === "") {
-    return "Invalid location";
-  }
-  const otherLocation = data?.location || "";
-  if (otherLocation === "") {
-    return "Location error";
-  }
+async function findLatLon(city) {
+  let latLon = { lat: "", lon: "" };
+  const geoAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=1560a07c19638ebfb003c32577cdfee1`;
+  let response = await fetch(geoAPI);
+  let data = await response.json();
+  latLon.lat = data[0].lat;
+  latLon.lon = data[0].lon;
+  return latLon;
+}
+
+async function distanceCalc(userLocation, otherLocation) {
   let location1 = userLocation.toLowerCase();
   location1 = location1.replace(" ", "_");
   let location2 = otherLocation.toLowerCase();
   location2 = location2.replace(" ", "_");
-  const userLatLon = findLatLon(location1);
-  const otherLatLon = findLatLon(location2);
+  const userLatLon = await findLatLon(location1);
+  const otherLatLon = await findLatLon(location2);
   const userLat = userLatLon.lat;
   const userLon = userLatLon.lon;
   const otherLat = otherLatLon.lat;
@@ -27,18 +29,19 @@ function Match() {
     { latitude: otherLat, longitude: otherLon }
   );
   distance = distance / 1609;
+  const finalDistance = Math.round(distance);
+  return finalDistance
 }
 
-function findLatLon(city) {
-  let latLon = { lat: "", lon: "" };
-  const geoAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=1560a07c19638ebfb003c32577cdfee1`;
-  fetch(geoAPI)
-    .then((response) => response.json())
-    .then((data) => {
-      latLon.lat = data[0].lat;
-      latLon.lon = data[0].lon;
-      return latLon;
-    });
+function Match() {
+  const user = auth.getProfile();
+  
+  console.log(user);
 }
+
+// findLatLon("fountain_valley");
+
+distanceCalc("Fountain Valley", "Irvine");
+Match();
 
 export default Match;
--- client/src/pages/Matches/practice.js
+++ client/src/pages/Matches/practice.js
@@ -1,14 +1,13 @@
-function findLatLon(city) {
+const { getDistance } = require("geolib");
+
+async function findLatLon(city) {
   let latLon = { lat: "", lon: "" };
   const geoAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=1560a07c19638ebfb003c32577cdfee1`;
-  fetch(geoAPI)
-    .then((response) => response.json())
-    .then((data) => {
-      latLon.lat = data[0].lat;
-      latLon.lon = data[0].lon;
-      console.log(latLon);
-      return latLon;
-    });
+  let response = await fetch(geoAPI);
+  let data = await response.json();
+  latLon.lat = data[0].lat;
+  latLon.lon = data[0].lon;
+  return latLon;
 }
 
 async function distanceCalc(userLocation, otherLocation) {
@@ -19,21 +18,19 @@ async function distanceCalc(userLocation, otherLocation) {
   location2 = location2.replace(" ", "_");
   console.log(location2);
   const userLatLon = await findLatLon(location1);
-  console.log(userLatLon);
-  // const otherLatLon = findLatLon(location2);
-  // console.log(otherLatLon);
-  // const userLat = userLatLon.lat;
-  // const userLon = userLatLon.lon;
-  // const otherLat = otherLatLon.lat;
-  // const otherLon = otherLatLon.lon;
-  // let distance = getDistance(
-  //   { latitude: userLat, longitude: userLon },
-  //   { latitude: otherLat, longitude: otherLon }
-  // );
-  // distance = distance / 1609;
-  // console.log(distance);
+  console.log("distance calc", userLatLon);
+  const otherLatLon = await findLatLon(location2);
+  console.log(otherLatLon);
+  const userLat = userLatLon.lat;
+  const userLon = userLatLon.lon;
+  const otherLat = otherLatLon.lat;
+  const otherLon = otherLatLon.lon;
+  let distance = getDistance(
+    { latitude: userLat, longitude: userLon },
+    { latitude: otherLat, longitude: otherLon }
+  );
+  distance = distance / 1609;
+  const finalDistance = Math.round(distance);
 }
-
-// findLatLon("fountain_valley");
 
 distanceCalc("Fountain Valley", "Irvine");
