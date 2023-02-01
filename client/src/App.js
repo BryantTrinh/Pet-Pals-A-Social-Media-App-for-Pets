ce9cc15482e3df53d479280c3f9a45f1809b4eee
monysary <sary.mony@gmail.com>
2023-02-01 11:28:21 -0800

    committing code

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,9 @@ import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Navbar from "./components/Navbar.js";
 import ChatBox from "./components/ChatBox.js";
-
-import { Backdrop } from "@mui/material";
+import Feed from './pages/Feed';
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -36,14 +33,6 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleToggle = () => {
-    setOpen(!open);
-  };
-
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -52,8 +41,7 @@ function App() {
           <ChatBox />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path='/Feed' element={<Feed />}/>
           </Routes>
         </div>
       </Router>
