6a92a9a175f70712a422945ce317652f8a2910b3
monysary <sary.mony@gmail.com>
2023-02-06 09:50:22 -0800

    comitting code

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://hidden-headland-00556.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
