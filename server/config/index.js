d3887d9a064864e41272db03aae706e6ba169f0a
Brian Wang <wangbrian26@gmail.com>
2023-02-06 01:32:11 -0800

    updated config

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || process.env.LOCAL_URI);
+mongoose.connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
