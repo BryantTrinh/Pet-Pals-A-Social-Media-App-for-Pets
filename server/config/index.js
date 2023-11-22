00216327288b6ae40ec07084be20a54821abaccc
Bryant Trinh <trinhbryant@gmail.com>
2023-11-21 21:02:45 -0800

    heroku server changes

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PetPalsDB';
 
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URI || process.env.LOCAL_URI, {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
