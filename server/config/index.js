523eac373c303799fc38d5c277f2881d7726a552
Bryant Trinh <trinhbryant@gmail.com>
2023-11-21 21:21:38 -0800

    new changes on heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PetPalsDB';
 
-console.log("Connecting to MongoDB with URI:", MONGODB_URI);
+console.log("Connecting to MongoDB with URI: txibxybxy7x4");
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
