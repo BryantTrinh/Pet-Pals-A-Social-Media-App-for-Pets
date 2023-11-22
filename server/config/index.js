3ae673b3e6411f9b6e6bf13ad751fd962d813a2c
Bryant Trinh <trinhbryant@gmail.com>
2023-11-21 21:05:48 -0800

    added console log heroku

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/PetPalsDB';
 
+console.log("Connecting to MongoDB with URI:", MONGODB_URI);
+
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB!');
+});
+
+module.exports = db;
